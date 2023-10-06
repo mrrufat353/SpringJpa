3f18e789459787c3d0e0156e4ed6965ef1fee4e1
mrrufat353 <ruafyseyidov@gmail.com>
2023-10-06 11:42:49 +0400

    Jpa is done

--- src/main/java/az/springjpa/controller/StudentController.java
+++ src/main/java/az/springjpa/controller/StudentController.java
@@ -1,6 +1,5 @@
 package az.springjpa.controller;
 
-import az.springjpa.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StudentController {
 
-    private final StudentRepository studentRepository;
 
     @GetMapping("/students")
     private String getStudents(
             @RequestParam(name = "firstName") String firstName,
-            @RequestParam(name = "lastName") String lastName,
+            @RequestParam(name = "secondName") String secondName,
             @RequestParam(name = "age") int age) {
-        return "First Name: " + firstName + ", Last Name: " + lastName + ", Age: " + age;
+        return "First Name: " + firstName + ", Second Name: " + secondName + ", Age: " + age;
     }
 }
